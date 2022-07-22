ba7960037ec1c934eddf75dcdafdad0d902f9fe4
azad <you@example.com>
2022-07-22 00:07:37 -0700

    I bring changes

--- src/pushCode/GitReporsitory.java
+++ src/pushCode/GitReporsitory.java
@@ -3,7 +3,10 @@ package pushCode;
 public class GitReporsitory {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [] arr= {1,3,5,2,8,4};
+		for(int i=0; i<5; i++)
+			System.out.println(arr[i]);
+			
 
 	}
 
