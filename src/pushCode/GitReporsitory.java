c7ce491ade9b78fd2aad7ff0af9d15cfe75c75f4
azad <you@example.com>
2022-07-22 22:45:40 -0700

    code updated

--- src/pushCode/GitReporsitory.java
+++ src/pushCode/GitReporsitory.java
@@ -4,10 +4,26 @@ public class GitReporsitory {
 
 	public static void main(String[] args) {
 		int [] arr= {1,3,5,2,8,4};
-		for(int i=0; i<5; i++)
-			System.out.println(arr[i]);
+		//for(int i=0; i<5; i++)
+			//System.out.println(arr[i]);
+		char s = 'a';
+		while (s<='z')
+		{
+		System.out.print(s+ " ");
+			s+=1;
+		}
+			char y = 'A';
+			while( y<='Z'){
+				System.out.print(y+ " ");
+				y+=1;
+				
+			
+				
+			}
+				
 			
 
 	}
 
 }
+
